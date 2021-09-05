6f5490f8d077a267dc76f58ec6b2384b940c3291
Juan Felipe Montaña Perdomo <juanmontana1398@gmail.com>
2021-09-05 12:09:51 -0500

    Skill section created and global styles files added to App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { Navbar } from "./components/Navbar/Navbar";
 import { Home } from "./components/Home/Home";
 import { About } from "./components/About/About";
+import { Skill } from "./components/Skill/Skill";
+import "./app.css";
 
 function App() {
   return (
@@ -8,6 +10,7 @@ function App() {
       <Navbar />
       <Home />
       <About />
+      <Skill />
     </>
   );
 }
