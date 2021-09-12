bd604cca7c05374f07174399570491ea07346ef8
Juan Felipe Montaña Perdomo <juanmontana1398@gmail.com>
2021-09-12 16:22:43 -0500

    TypingAnimation created and incoporated to home component

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,8 +2,10 @@ import React from "react";
 import { Effects } from "./Effects/Effects";
 import "./home.css";
 import profile from "../../assets/home_img.jpg";
+import { TypingAnimation } from "../shared/TypingAnimation/TypingAnimation";
 
 export function Home() {
+  const typingPhrases = ["Web Developer", "Mobile Developer", "Freelancer"];
   return (
     <>
       <Effects />
@@ -13,7 +15,7 @@ export function Home() {
             <div className="text-1">Hello, my name is</div>
             <div className="text-2">Juan Felipe</div>
             <div className="text-3">
-              And I'm a <span className="typing"></span>
+              And I'm a <TypingAnimation phrases={typingPhrases} />
             </div>
           </div>
           <div className="image-wrapper">
