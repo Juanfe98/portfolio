cb107c2cf29db6b04e207dead7208c19e04f0b82
Juan Felipe Montaña Perdomo <juanmontana1398@gmail.com>
2021-09-13 23:36:19 -0500

    Navigation bar modificated for responsive design and implementation of useRef to toggle the class of an element

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,15 +1,21 @@
-import React from "react";
-import styles from "./styles.module.css";
+import React, { useRef } from "react";
+import "./styles.css";
+
 export function Navbar() {
+  const menuRef = useRef(null);
+  const showMenu = () => {
+    console.log(menuRef.current.classList);
+    menuRef.current.classList.toggle("active");
+  };
   return (
-    <nav className={styles.navbar}>
-      <div className={styles.maxWidth}>
-        <div className={styles.logo}>
+    <nav className="navbar">
+      <div className="maxWidth">
+        <div className="logo">
           <a href="#">
             Port<span>folio</span>
           </a>
         </div>
-        <ul className={styles.menu}>
+        <ul ref={menuRef} className="menu">
           <li>
             <a href="#">Home</a>
           </li>
@@ -29,6 +35,9 @@ export function Navbar() {
             <a href="#">Contact</a>
           </li>
         </ul>
+        <div className="menuBtn" onClick={() => showMenu()}>
+          <i className="fas fa-bars"> </i>
+        </div>
       </div>
     </nav>
   );
